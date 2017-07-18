f6320256b04ba4eeec12f00254d756c5736606d9
Seducer_star <243924313@qq.com>
2017-07-18 17:59:19 +0800

    scorller
    拖动菜单栏改变类别activity

--- pages/component/mall/mall.js
+++ pages/component/mall/mall.js
@@ -3,22 +3,14 @@ var menu = require("../../common/menu.js");
 Page({
   data: {
     height: 0,
-    indicatorDots: true,
-    mode: 'aspectFit',
-    autoplay: true,
-    interval: 3000,
-    duration: 1000,
     menu_list: [],
     scrollTop: 100,
     curIndex: 1,
-    toView: ''
+    toView: '',
+    listsHeight: [],
+    unitPx: 0.5,
+    toViewLeft: '' 
   },
-  switchTab: function (e) {
-    this.setData({
-      curIndex: e.target.dataset.id,
-      toView: e.target.dataset.value
-    })
-  }, 
   /**
    * 生命周期函数--监听页面加载
    */
@@ -27,13 +19,53 @@ Page({
     self.setData({
       menu_list: menu
     });
-    console.log(self.data.menu_list);
     wx.getSystemInfo({
       success: function (res) {
         self.setData({
-          height: res.windowHeight
+          height: res.windowHeight,
+          unitPx: res.windowWidth / 750
         });
       }
+    }); 
+  },
+  onReady: function (options) {
+    var list = this.getListHeight(menu, this.data.unitPx);
+    this.setData({
+      listsHeight: list
+    });
+  },
+  switchTab: function (e) {
+    this.setData({
+      curIndex: e.target.dataset.id,
+      toView: e.target.dataset.value
     })
+  },
+  scroll: function (e) {
+    console.log(e.detail.scrollTop);
+    var heights = this.data.listsHeight;
+    var tempValue, tempId;
+    for (var i in heights) {
+      if (e.detail.scrollTop >= heights[i].height){
+        tempValue = heights[i].value;
+        tempId = heights[i].id;
+      }
+    }
+    this.setData({
+      curIndex: tempId,
+      toViewLeft: tempValue
+    });
+  },
+  getListHeight: function (arr, unit) {
+    var kidsLength = 0; //获取该列子元素的长度
+    for (var i in arr){
+      if (i == 0) {
+        kidsLength = arr[i].food.length;
+        continue;
+      }
+      arr[i].height = arr[i - 1].height+(kidsLength * 130 + 50) * unit; 
+      kidsLength = arr[i].food.length;
+    }
+    console.log(arr);
+    return arr;
   }
 })
--- pages/component/personalCenter/personalCenter.js
+++ pages/component/personalCenter/personalCenter.js
@@ -19,11 +19,11 @@ Page({
   },
   onLoad: function () {
     console.log('onLoad')
-    var that = this
+    var self = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
       //更新数据
-      that.setData({
+      self.setData({
         userInfo: userInfo
       })
     })
